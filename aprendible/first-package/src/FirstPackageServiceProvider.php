ac792baff8981a55d8d240c6277e4c897200908c
nguyenvd <nguyenvd@tcv.vn>
2023-05-29 22:26:45 +0700

    allow overwrite views package

--- aprendible/first-package/src/FirstPackageServiceProvider.php
+++ aprendible/first-package/src/FirstPackageServiceProvider.php
@@ -10,6 +10,10 @@ class FirstPackageServiceProvider extends ServiceProvider
     public function boot(){
 //        $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
         $this->loadViewsFrom(__DIR__.'/../resources/views', 'first-package');
+
+        $this->publishes([
+            __DIR__.'/../resources/views'=>resource_path('views/vendor/first-package')
+        ],'first-package-views');
     }
 
     public function register()
---
+++ package-tester/resources/views/vendor/first-package/home.blade.php
@@ -0,0 +1,6 @@
+@extends('first-package::layout')
+@section('content')
+    <div>
+        <h2>{{$message}}</h2>
+    </div>
+@endsection
